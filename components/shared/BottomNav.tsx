4e3abf0f306b1c44c67397a33727dd3b4d77495b
KavishkaPC <KavishkaPC@KAVISHKAPC>
2025-08-20 22:15:17 +0530

    1st Up

---
+++ components/shared/BottomNav.tsx
@@ -0,0 +1,40 @@
+import { AlignJustify, CircleEllipsis, Home, Icon } from "lucide-react";
+import React from "react";
+import { Button } from "../ui/button";
+import { chairsTablePlatter, bellConcierge } from "@lucide/lab";
+
+const BottomNav = () => {
+  return (
+    <div className="fixed bottom-0 left-0 right-0 pt-4 p-2 h-16 flex justify-around shadow-[0_-4px_6px_rgba(0,0,0,0.1)]">
+      <Button
+        className={` flex items-center justify-center font-bold w-[300px]   rounded-3xl bg-!none text-muted-foreground shadow-none hover:bg-transparent hover:text-accent-foreground`}
+      >
+        <Home className="inline mr-2 !w-5 !h-5" />
+        <p className="text-lg">Home</p>
+      </Button>
+      <Button
+        className={`flex items-center justify-center font-bold w-[300px] rounded-3xl bg-!none text-muted-foreground shadow-none hover:bg-transparent hover:text-accent-foreground `}
+      >
+        <AlignJustify className="inline mr-2 !w-5 !h-5" />{" "}
+        <p className="text-lg">Orders</p>
+      </Button>
+      <Button
+        className={`flex items-center justify-center font-bold $ w-[300px]rounded-3xl bg-!none text-muted-foreground shadow-none hover:bg-transparent hover:text-accent-foreground `}
+      >
+        <Icon iconNode={chairsTablePlatter} className="inline mr-2 !w-6 !h-6" />
+        <p className="text-lg">Tables</p>
+      </Button>
+
+      <Button className="flex items-center justify-center font-bold  w-[300px] rounded-3xl bg-!none text-muted-foreground shadow-none hover:bg-transparent hover:text-accent-foreground">
+        <CircleEllipsis className="inline mr-2 !w-5 !h-5" />{" "}
+        <p className="text-lg">More</p>
+      </Button>
+
+      <Button className="absolute bottom-7 rounded-full w-20 h-20 flex items-center justify-center p-0">
+        <Icon iconNode={bellConcierge} className="!w-10 !h-10" />
+      </Button>
+    </div>
+  );
+};
+
+export default BottomNav;
---
+++ components/shared/Header.tsx
@@ -0,0 +1,45 @@
+import { Bell, ChefHat, Search, User } from "lucide-react";
+import React from "react";
+import { Input } from "../ui/input";
+import { cn } from "@/lib/utils";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="flex justify-between items-center py-4 px-8 bg-background shadow-md">
+        {/* LOGO */}
+        <div className="flex items-center gap-2 cursor-pointer">
+          <ChefHat />
+          {/*<img className="h-8 w-8" alt="OrdiTrak logo" />*/}
+          <h1 className="text-lg font-semibold tracking-wide">OrdiTrak</h1>
+        </div>
+
+        {/* SEARCH */}
+        <div className="relative w-[500px]">
+          <Input
+            type="text"
+            placeholder="Search"
+            className="rounded-full py-5 px-10"
+          />
+          <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground" />
+        </div>
+
+        {/* LOGGED USER DETAILS */}
+        <div className="flex items-center gap-4">
+          <div className="bg-muted rounded-2xl p-3 cursor-pointer">
+            <Bell className="text-2xl" />
+          </div>
+          <div className="flex items-center gap-3 cursor-pointer ">
+            <User className="text-4xl" />
+            <div className="flex flex-col items-start">
+              <h1 className="text-md font-semibold tracking-wide">Kasun</h1>
+              <p className="text-xs text-muted-foreground font-medium">Admin</p>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
